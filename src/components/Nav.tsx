c0174ffac9452b30aca8a0737502cd5a512c3afa
romainAccoce <romainaccoce@gmail.com>
2024-11-04 16:14:04 -0500

    Linter configuration

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from "react-router-dom";
 
 function Nav() {
   return (
     <>
-        <ul>
-            <li><Link to={'projects'}>Projects</Link></li>
-            <li><Link to={'informations'}>Informations</Link></li>
-            <li><Link to={'users'}>Users</Link></li>
-        </ul>
-        <Outlet />
+      <ul>
+        <li>
+          <Link to={"projects"}>Projects</Link>
+        </li>
+        <li>
+          <Link to={"informations"}>Informations</Link>
+        </li>
+        <li>
+          <Link to={"users"}>Users</Link>
+        </li>
+      </ul>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
